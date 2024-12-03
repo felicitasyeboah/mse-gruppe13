2ec47911d6fa70ff59586496c44efde971a080a2
RoxiBu <roxibu111@gmail.com>
2024-12-03 14:47:36 +0100

    Revert "Add Logging"
    This reverts commit 93486fb0e40d0a737a5ea5e814ea8e244d9c13e2.

--- src/main/java/de/cityfeedback/feedbackverwaltung/domain/listener/FeedbackCreatedEventListener.java
+++ src/main/java/de/cityfeedback/feedbackverwaltung/domain/listener/FeedbackCreatedEventListener.java
@@ -1,20 +1,24 @@
 package de.cityfeedback.feedbackverwaltung.domain.listener;
 
 import de.cityfeedback.feedbackverwaltung.domain.events.FeedbackCreatedEvent;
+import java.util.logging.Logger;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedbackCreatedEventListener {
+  Logger logger = Logger.getLogger(FeedbackCreatedEventListener.class.getName());
 
-    @EventListener
-    public void handleFeedbackCreatedEvent(FeedbackCreatedEvent event) {
-        // Die Geschäftslogik für die Verarbeitung des Feedback-Ereignisses
-        System.out.println("A new feedback has been created: " + event.getFeedbackId() + " - " + event.getTitle());
-
-        // Weitere Aktionen, z. B.:
-        // - Update von Read-Modellen
-        // - Senden von Benachrichtigungen
-        // - Interaktion mit externen Systemen
-    }
+  @EventListener
+  public void handleFeedbackCreatedEvent(FeedbackCreatedEvent event) {
+    // React to the event, such as updating a read model, logging, or sending notifications
+    String msg =
+        "A new feedback has been created: " + event.getFeedbackId() + " - " + event.getTitle();
+    logger.info(msg);
+    System.out.println(msg);
+    // Handle the event, for example:
+    // - Update projections
+    // - Send an email notification
+    // - Trigger external system interactions
+  }
 }
