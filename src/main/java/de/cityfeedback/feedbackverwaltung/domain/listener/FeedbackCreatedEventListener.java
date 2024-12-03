a74ab8768fc1a383fde16178cf0284330a862ada
RoxiBu <roxibu111@gmail.com>
2024-12-03 15:32:27 +0100

    add AOP aspect Logging

--- src/main/java/de/cityfeedback/feedbackverwaltung/domain/listener/FeedbackCreatedEventListener.java
+++ src/main/java/de/cityfeedback/feedbackverwaltung/domain/listener/FeedbackCreatedEventListener.java
@@ -1,24 +1,25 @@
 package de.cityfeedback.feedbackverwaltung.domain.listener;
 
 import de.cityfeedback.feedbackverwaltung.domain.events.FeedbackCreatedEvent;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedbackCreatedEventListener {
-  Logger logger = Logger.getLogger(FeedbackCreatedEventListener.class.getName());
+  
+  private static final Logger logger = LoggerFactory.getLogger(FeedbackCreatedEventListener.class);
 
   @EventListener
   public void handleFeedbackCreatedEvent(FeedbackCreatedEvent event) {
-    // React to the event, such as updating a read model, logging, or sending notifications
+    // Reagiere auf das Ereignis, wie z.B. das Aktualisieren eines Read Models, Logging oder Benachrichtigungen
     String msg =
         "A new feedback has been created: " + event.getFeedbackId() + " - " + event.getTitle();
     logger.info(msg);
-    System.out.println(msg);
-    // Handle the event, for example:
-    // - Update projections
-    // - Send an email notification
-    // - Trigger external system interactions
+    // Handle das Ereignis, z.B.:
+    // - Projections aktualisieren
+    // - E-Mail-Benachrichtigung senden
+    // - Externe Systeminteraktionen auslösen
   }
 }
