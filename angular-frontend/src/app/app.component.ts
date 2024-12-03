49edb9f07e294bf0e4b8cd0536d71ecb5d5e630e
RoxiBu <roxibu111@gmail.com>
2024-12-03 14:06:33 +0100

    add angular components

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ComplaintsListComponent } from "./complaints-list/complaints-list.component";
+import { ImpressumComponent } from "./impressum/impressum.component";
+import { ComplaintFormComponent } from "./complaint-form/complaint-form.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ComplaintsListComponent, ImpressumComponent, ComplaintFormComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'angular-frontend';
