c07cd4572f87989f0dad7f83dc43d25527268a68
christineneick <christine.neick@gmail.com>
2025-01-02 12:10:18 +0100

    Datenabfragen in UserRepository

--- src/main/java/de/cityfeedback/userverwaltung/infrastructure/repositories/UserRepository.java
+++ src/main/java/de/cityfeedback/userverwaltung/infrastructure/repositories/UserRepository.java
@@ -1,20 +1,21 @@
 package de.cityfeedback.userverwaltung.infrastructure.repositories;
 
 import de.cityfeedback.userverwaltung.domain.model.User;
-import de.cityfeedback.userverwaltung.domain.valueobject.*;
 
-import static de.cityfeedback.userverwaltung.domain.valueobject.Role.CITIZEN;
-import static de.cityfeedback.userverwaltung.domain.valueobject.UserStatus.AKTIV;
-
-import de.cityfeedback.userverwaltung.domain.model.User;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
-public interface UserRepository {
-    User findUserByEmail(String email);
+    @Query("SELECT us FROM User us WHERE LOWER(us.email) = LOWER(:email)")
+    Optional<User> findUserByEmail(@Param("email") String email);
+
+    @Query("SELECT us FROM User us WHERE us.EMAIL = :email AND us.PASSWORD = :password")
+    Optional<User> findUserByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
 }
-
-
