2aa1a94954368d052137b9eb62c63f9a50ab9cd5
Felicitas Yeboah <64726454+felicitasyeboah@users.noreply.github.com>
2025-01-03 13:07:46 +0100

    fix domain event logging aspect

--- src/main/java/de/cityfeedback/feedbackverwaltung/domain/listener/FeedbackCreatedEventListener.java
+++ src/main/java/de/cityfeedback/feedbackverwaltung/domain/listener/FeedbackCreatedEventListener.java
@@ -1,22 +1,17 @@
 package de.cityfeedback.feedbackverwaltung.domain.listener;
 
 import de.cityfeedback.feedbackverwaltung.domain.events.FeedbackCreatedEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedbackCreatedEventListener {
-  
-  private static final Logger logger = LoggerFactory.getLogger(FeedbackCreatedEventListener.class);
 
   @EventListener
-  public void handleFeedbackCreatedEvent(FeedbackCreatedEvent event) {
-    // Reagiere auf das Ereignis, wie z.B. das Aktualisieren eines Read Models, Logging oder Benachrichtigungen
-    String msg =
-        "A new feedback has been created: " + event.getFeedbackId() + " - " + event.getTitle();
-    logger.info(msg);
+  public String handleFeedbackCreatedEvent(FeedbackCreatedEvent event) {
+    // Reagiere auf das Ereignis, wie z.B. das Aktualisieren eines Read Models, Logging oder
+    // Benachrichtigungen
+    return "A new feedback has been created: " + event.getFeedbackId() + " - " + event.getTitle();
     // Handle das Ereignis, z.B.:
     // - Projections aktualisieren
     // - E-Mail-Benachrichtigung senden
---
+++ src/main/java/de/cityfeedback/feedbackverwaltung/infrastructure/aspects/DomainEventLoggingAspect.java
@@ -0,0 +1,43 @@
+package de.cityfeedback.feedbackverwaltung.infrastructure.aspects;
+
+import java.util.Arrays;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class DomainEventLoggingAspect {
+
+  private static final Logger logger = LoggerFactory.getLogger(DomainEventLoggingAspect.class);
+
+  // Definiere ein Pointcut für alle Methoden in der Service-Schicht
+  @Pointcut("execution(* de.cityfeedback.feedbackverwaltung.domain.listener.*.*(..))")
+  public void domainEventHandler() {}
+
+  // Logging vor der Methode
+  @Before("domainEventHandler()")
+  public void logBefore(JoinPoint joinPoint) {
+    logger.info("Methode aufgerufen: " + joinPoint.getSignature().getName());
+    logger.info("Argumente: " + Arrays.toString(joinPoint.getArgs()));
+  }
+
+  // Logging nach der Methode (Erfolgreiche Rückkehr)
+  @AfterReturning(value = "domainEventHandler()", returning = "result")
+  public void logAfterReturning(JoinPoint joinPoint, Object result) {
+    logger.info("Methode zurückgegeben: " + joinPoint.getSignature().getName());
+    logger.info("Ergebnis: " + result);
+  }
+
+  // Logging im Falle einer Ausnahme
+  @AfterThrowing(value = "domainEventHandler()", throwing = "exception")
+  public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+    logger.error("Fehler in Methode: " + joinPoint.getSignature().getName(), exception);
+  }
+}
