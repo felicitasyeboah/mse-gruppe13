f69f3608399c0faf2a1a878db93b28503805d137
Felicitas Yeboah <64726454+felicitasyeboah@users.noreply.github.com>
2025-01-06 20:43:04 +0100

    add new example page and add it to navigation

---
+++ angular-frontend/src/app/feedback-example/feedback-example/feedback-example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-example',
+  imports: [],
+  templateUrl: './feedback-example.component.html',
+  styleUrl: './feedback-example.component.css'
+})
+export class FeedbackExampleComponent {
+
+}
