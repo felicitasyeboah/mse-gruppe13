f174836dd050be3f6f53030fa04e2d1ea19c308b
katicookie <katkno146@gmail.com>
2025-01-08 00:39:48 +0100

    Open Feedback unter /open-feedback und Liste angepasst:)

---
+++ angular-frontend/src/app/open-feedback/open-feedback.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import {ApiService} from '../services/api.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-open-feedback',
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './open-feedback.component.html',
+  styleUrl: './open-feedback.component.css'
+})
+export class OpenFeedbackComponent {
+  data: any; // to store the fetched data
+  errorMessage: string = ''; // to store any error message
+
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Call the service method to fetch data
+    const userId = 1; // TODO: get the user id from the logged in user
+    // TODO: If User Role = Admin, then ...
+    this.apiService.fetchUserFeedbacks(userId).subscribe(
+      (response) => {
+        this.data = response; // Handle the successful response
+        console.log(this.data);
+      },
+      (error) => {
+        this.errorMessage = error.message; // Handle the error
+        console.error(error);
+      },
+    );
+  }
+  goToDetail(id: string): void {
+    this.router.navigate(['/feedback', id]);
+  }
+
+}
