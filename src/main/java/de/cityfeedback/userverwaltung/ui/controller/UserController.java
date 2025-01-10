0aeaaad9808fd0200ba65fd54ed0cb62463d87e5
christineneick <christine.neick@gmail.com>
2025-01-10 17:30:13 +0100

    add UserResponse

--- src/main/java/de/cityfeedback/userverwaltung/ui/controller/UserController.java
+++ src/main/java/de/cityfeedback/userverwaltung/ui/controller/UserController.java
@@ -1,5 +1,9 @@
 package de.cityfeedback.userverwaltung.ui.controller;
 
+import de.cityfeedback.feedbackverwaltung.application.dto.ApiResponse;
+import de.cityfeedback.feedbackverwaltung.application.dto.FeedbackResponse;
+import de.cityfeedback.shared.validator.Validation;
+import de.cityfeedback.userverwaltung.application.dto.UserResponse;
 import de.cityfeedback.userverwaltung.application.services.UserService;
 import de.cityfeedback.userverwaltung.domain.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,10 +24,17 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String password, User user) {
-        boolean loginSuccessful = userService.loginUser(email, password);
+    public String login(@RequestParam String email, @RequestParam String password) {
+        //Exception-Handling
+        //leere Eingaben abfangen?
+
+        boolean loginSuccessful = userService.authenticateUser(email, password);
         System.out.println("Controller Login: " + email + " " + password);
-        return loginSuccessful ? "Login erfolgreich" + user.toString() : "Login fehlgeschlagen";
+
+        User user = userService.findUser(email); // Benutzer aus der Datenbank abrufen
+        UserResponse userResponse = UserResponse.fromUser(user);
+        ApiResponse response = new ApiResponse("Erfolgreich eingeloggt mit E-Mail: " +  user.getEmail(), userResponse);
+        return loginSuccessful ? "Login erfolgreich, User: " + user.toString() : "Login fehlgeschlagen";
     }
 
 }
