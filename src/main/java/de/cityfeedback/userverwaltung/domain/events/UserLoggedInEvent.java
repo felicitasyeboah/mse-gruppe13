259808eda9cc944692539ecc900003767983b733
christineneick <christine.neick@gmail.com>
2025-01-26 16:48:24 +0100

    add Tests UserLoggedInEvent

--- src/main/java/de/cityfeedback/userverwaltung/domain/events/UserLoggedInEvent.java
+++ src/main/java/de/cityfeedback/userverwaltung/domain/events/UserLoggedInEvent.java
@@ -3,22 +3,23 @@ package de.cityfeedback.userverwaltung.domain.events;
 import de.cityfeedback.shared.events.DomainEvent;
 import de.cityfeedback.userverwaltung.domain.valueobject.Role;
 import java.time.Instant;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
 public class UserLoggedInEvent extends DomainEvent {
-
-  Long userId;
-  String email;
-  String userName;
-  Role role;
+  private Long userId;
+  private String email;
+  private String userName;
+  private Role role;
   private final Instant registeredAt;
 
-  public UserLoggedInEvent(long id, String userName, String email, Role role) {
+  public UserLoggedInEvent(long userId, String userName, String email, Role role) {
+    this.userId = userId;
+    this.userName = userName;
+    this.email = email;
+    this.role = role;
     this.registeredAt = Instant.now();
   }
 }
